4384fc53452016c93fd114a26959cf5dac13f325
nikeiishaa <kirananikeisha99@gmail.com>
2026-01-14 14:49:19 +0700

    menambahkan parameter dan argumen

--- Program.cs
+++ Program.cs
@@ -1,28 +1,31 @@
-﻿int a = 10, b = 2, c = 0;
+﻿int c = 0;
 
-void tambah ()
+void tambah (int a, int b)
 {
     c = a + b;
     Console.WriteLine($"{a} + {b} = {c}");
 }
-void kurang()
+void kurang(int a, int b)
 {
     c = a - b;
     Console.WriteLine($"{a} - {b} = {c}");
 }
-void kali()
+void kali(int a, int b)
 {
     c = a * b;
     Console.WriteLine($"{a} * {b} = {c}");
 }
-void bagi()
+void bagi(int a, int b)
 {
     c = a / b;
     Console.WriteLine($"{a} / {b} = {c}");
 }
 
-tambah();
-kurang();
-tambah();
-kali();
-bagi();
+tambah(6, 7);
+kurang(19, 4);
+tambah(20, 9);
+kali(2, 5);
+bagi(50, 5);
+
+
+
